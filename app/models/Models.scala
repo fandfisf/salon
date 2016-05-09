caf8c9634e041d719b74f85eeaab7fd2649ae283
Prashant-learner <fandfisf@gmail.com>
2016-05-08 23:01:10 -0700

    Add version to Painter for optimistic locking

--- app/models/Models.scala
+++ app/models/Models.scala
@@ -8,4 +8,10 @@ import anorm.Macro
 case class Painter(painterId: Long,  firstName: String, lastName: String, pseudonym: String,  picture: String, version:Int)
 object Painter {
   def parser = Macro.parser[Painter] ("painter_Id", "first_Name", "last_Name", "pseudonym", "picture", "version")
+  implicit def  painterToMapEntry (p:Painter) : (Long, Painter) = {
+    p.painterId -> p
+  }
+  implicit def  painterToId (p:Painter) : Long = {
+    p.painterId
+  }
 }
